7b7403ff252f7c898687abea37dd502cf2695bbf
Vyacheslav Vyatkin <Vyatkin.vyacheslav.a@yandex.ru>
2023-12-23 22:20:24 +0500

    commit 2

---
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -0,0 +1,38 @@
+package ru.netology.testmode.test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.testmode.data.DataGenerator.Registration.getUser;
+
+class AuthTest {
+
+    @BeforeEach
+    void setup() { open("http://localhost:9999"); }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("h2").shouldHave(Condition.exactText("Личный кабинет")).shouldBe(Condition.visible);
+    }
+
+   // @Test
+    //@DisplayName("Should get error message if login with not registered user")
+   // void shouldGetErrorIfNotRegisteredUser() {
+       // var notRegisteredUser = getUser("active");
+       // $("[data-test-id='login'] input").setValue(notRegisteredUser.getLogin());
+       // $("[data-test-id='password'] input").setValue(notRegisteredUser.getPassword());
+
+    
+
+
+}
